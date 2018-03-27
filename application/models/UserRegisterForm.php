0a718229bfb742f4c84bd949a3a1cdd1eaec61cc
chain24 <chainsy97@gmail.com>
2018-03-27 16:08:29 +0800

    create register form

--- application/models/UserRegisterForm.php
+++ application/models/UserRegisterForm.php
@@ -8,5 +8,24 @@
 
 class UserRegisterForm extends Zend_Form
 {
-    
+    public function __construct($options=null)
+    {
+        parent::__construct($options);
+        $this->setName('registerForm');
+        $username = new Zend_Form_Element_Text('username');
+        $username->setLabel('用户名：')
+            ->setRequired(true)
+            ->addFilter('StripTags')
+            ->addFilter('StringTrim')
+            ->addValidator('NotEmpty');
+        $password = new Zend_Form_Element_Password('password');
+        $password->setLabel('密 码：')
+            ->setRequired(true)
+            ->addFilter('StripTags')
+            ->addFilter('StringTrim')
+            ->addValidator('NotEmpty');
+        $submit = New Zend_Form_Element_Submit('submit');
+        $submit->setAttrib('id','registerbutton')->setLabel('注册');
+        $this->addElements(array($username, $password, $submit));
+    }
 }
