2c58b4fe7eaa9f84698c082c608b8899077d43e5
chain24 <chainsy97@gmail.com>
2018-03-28 09:38:19 +0800

    change sql from * to 1

--- application/models/User.php
+++ application/models/User.php
@@ -10,10 +10,23 @@ class User extends Zend_Db_Table
 {
     protected $_name = 'user';
 
+    /**
+     * 检验用户的唯一性
+     * @param $name
+     * @return bool
+     */
     public function checkUnique($name)
     {
         $select = $this->_db->select()->from($this->_name)->where('username=?',$name);
         if($this->getAdapter()->fetchOne($select))
+            return true;
+        else
+            return false;
+    }
+    public function authCheck($name,$password)
+    {
+        $select = "SELECT 1 FROM `user` WHERE `username`='$name' AND `password`='$password'";
+        if($this->_db->fetchOne($select))
             return true;
         else
             return false;
