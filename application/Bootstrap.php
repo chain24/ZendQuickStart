b14dd17c709dc46ab2660e25ddab486fe3c540d6
chain24 <chainsy97@gmail.com>
2018-03-28 15:46:31 +0800

    add session params set

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,6 +2,12 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
+    protected function _initSession()
+    {
+        $options = $this->getOption('resource');
+        $option = $options['session'];
+        Zend_Session::setOptions($option);
+        Zend_Session::start();
+    }
 }
 
